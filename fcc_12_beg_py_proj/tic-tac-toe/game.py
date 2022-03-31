2c80604bf40650922cc6c71850428a6b828ddee0
Rhuck1 <bobbyhuck@gmail.com>
2022-03-30 17:33:13 -0700

    can't save pw in cache

--- fcc_12_beg_py_proj/tic-tac-toe/game.py
+++ fcc_12_beg_py_proj/tic-tac-toe/game.py
@@ -48,8 +48,30 @@ class TicTacToe:
             return True
         return False
     
-    def winner():
-        pass
+    def winner(self, winner):
+        # winner if 3 in a row anywhere...we have to check all of these!
+        # first let's check the row
+        row_in = square // 3
+        row = self.board[row_ind * 3: (row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+        
+        # check column
+        col_in = square % 3
+        column = [self.board[col_ind + i * 3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+        
+        # check diagonals
+        # but only if the square is an even number (0, 2, 4, 6, 8)
+        # these are the only moves possible to win a diagonal
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]] # left to right diagonal
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]] # right to left diagonal
+            if all([spot == letter for spot in diagonal2]):
+                return True
 
 def play(game, x_player, o_player, print_game=True):
     # returns the winner of the game(the letter)! or None for a tie
